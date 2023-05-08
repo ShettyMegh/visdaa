662af2a779a959df12fc1e9f112875a423a924c0
ShettyMegh <meghanathshetty108@gmail.com>
2023-05-09 00:02:40 +0530

    removefirst and removelast functionality added

--- data-structures/linked-list/sll/sll.js
+++ data-structures/linked-list/sll/sll.js
@@ -1,6 +1,5 @@
 //create node element
 function createNode(input) {
-  console.log(input);
   const nodeCard = document.createElement("div");
   nodeCard.classList.add("node-card");
   const node = document.createElement("div");
@@ -14,24 +13,11 @@ function createNode(input) {
   return nodeCard;
 }
 
-//appending
-function prepend(input) {
-  const node = createNode(input);
-  const nodeNull = document.querySelector("#node-null");
-  nodeNull.parentNode.insertBefore(node, nodeNull);
-}
-
-function append(input) {
-  const node = createNode(input);
-  const nodeHead = document.querySelector("#node-head");
-  nodeHead.parentNode.insertBefore(node, nodeHead.nextSibling);
-}
 
 const input = document.querySelector("#input");
 
 // disable add btns if no value in input
 input.addEventListener("input", (e) => {
-  console.log(e.currentTarget.value);
   if (e.currentTarget.value) {
     toggleDisableBtns(false);
   } else {
@@ -58,6 +44,34 @@ function getNodeLength() {
 
 const btns = document.querySelectorAll("#controls button");
 
+//ADT functions
+function append(input) {
+  const node = createNode(input);
+  const visualContainer = document.querySelector(".visual-container");
+  console.log("sdasd")
+  visualContainer.insertBefore(node,visualContainer.children[getNodeLength() - 1]);
+}
+
+function prepend(input) {
+  const node = createNode(input);
+  const visualContainer = document.querySelector(".visual-container");
+  visualContainer.insertBefore(node,visualContainer.children[1]);
+
+}
+
+function removeFirst(){
+  const nodes = document.querySelectorAll(".node-card");
+  console.log(nodes[1])
+  if(getNodeLength() > 2) nodes[1].remove();
+}
+
+function removeLast(){
+  const nodes = document.querySelectorAll(".node-card");
+  const nodeLen = getNodeLength();
+  if(nodeLen > 2) nodes[nodeLen-2].remove();
+  toggleDisableBtns(false);
+}
+
 // ADT btns operations
 btns.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -69,7 +83,7 @@ btns.forEach((btn) => {
         break;
 
       case "removeFirst":
-        console.log("rmf");
+          removeFirst();
         break;
 
       case "addLast":
@@ -78,8 +92,8 @@ btns.forEach((btn) => {
         toggleDisableBtns(true);
         break;
 
-      case "removeFirst":
-        console.log("rmf");
+      case "removeLast":
+        removeLast();
         break;
       default:
         console.log("none");
